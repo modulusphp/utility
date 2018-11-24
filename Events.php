04105ab6b9a5db60fce6d263c4b6c64532651adc
Donald Pakkies <donaldpakkies@gmail.com>
2018-11-24 15:37:45 +0200

    Feat: added support for grouped events and closure events

--- Events.php
+++ Events.php
@@ -12,6 +12,13 @@ class Events
   protected static $events = [];
 
   /**
+   * $register
+   *
+   * @var array
+   */
+  public $register = [];
+
+  /**
    * listen to event
    *
    * @param string $name
@@ -24,10 +31,10 @@ class Events
   }
 
   /**
-   * trigger event
+   * trigger an event
    *
-   * @param string $name
-   * @param mixed ?array
+   * @param string     $name
+   * @param null|array $args
    * @return void
    */
   public static function trigger(string $name, ?array $args = null)
@@ -38,10 +45,47 @@ class Events
       if (is_callable($callback)) {
         return call_user_func_array($callback, isset($args) ? $args : []);
       }
+      else if (is_array($callback)) {
+        (new Events)->triggerGroup($callback, $args);
+      }
       else if (is_string($callback)) {
         $call = new $callback;
         return call_user_func_array([$call, 'persist'], $args ?? []);
       }
+    }
+  }
+
+  /**
+   * Trigger a grouped event
+   *
+   * @param mixed ?array
+   * @param mixed ?array
+   * @return void
+   */
+  public function triggerGroup(?array $events = [], ?array $args = null)
+  {
+    foreach($events as $callback) {
+      if (is_callable($callback)) {
+        call_user_func_array($callback, isset($args) ? $args : []);
+      }
+      else if (is_string($callback)) {
+        $call = new $callback;
+        call_user_func_array([$call, 'persist'], $args ?? []);
+      }
+    }
+  }
+
+  /**
+   * Register events
+   *
+   * @return void
+   */
+  public function register()
+  {
+    $events = $this->register;
+
+    foreach($events as $name => $event) {
+      Events::listen($name, $event);
     }
   }
 }
